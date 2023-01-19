83ae659eb4befa4ff289a00e4c948dc61d59deb2
mdmdr8 <89672780+mdmdr8@users.noreply.github.com>
2023-01-19 21:46:07 +0900

    문자열

---
+++ Bronze/Lv2. 문자열 반복.py
@@ -0,0 +1,20 @@
+t = int(input())
+
+for _ in range(t):
+    r, s = input().split()
+    p = []
+    for i in s:
+
+        for _ in range(int(r)):
+            p.append(i)
+    print(''.join(p))
+
+# #study
+# t = int(input())
+
+# for _ in range(t):
+#     r, s = input().split()
+#     for x in s:
+#         print(x*int(r), end='')
+#     print() #줄넘김
+    
