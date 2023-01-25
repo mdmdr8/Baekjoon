225f52c7bc1310d996f99aac0decb58065e12475
mdmdr8 <89672780+mdmdr8@users.noreply.github.com>
2023-01-25 22:53:46 +0900

    버블 정렬 응용

---
+++ Bronze/Lv2. 최대값.py
@@ -0,0 +1,15 @@
+arr = [list(map(int, input().split())) for _ in range(9)]
+
+temp = 0
+n, m = 0, 0
+for i in range(9):
+    for j in range(9):
+        if arr[i][j] >  temp :
+            temp = arr[i][j]
+            n, m = i, j
+        else:
+            temp = temp
+
+
+print(temp)
+print(n+1, m+1)
