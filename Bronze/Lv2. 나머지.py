cc5599770eb1a4e5ff1d98c5ddd62ad742065c8b
mdmdr8 <89672780+mdmdr8@users.noreply.github.com>
2023-02-01 03:36:03 +0900

    1차원 배열

---
+++ Bronze/Lv2. 나머지.py
@@ -0,0 +1,26 @@
+array = []
+value = []
+cnt = 0
+for _ in range(10):
+    array.append(int(input()))
+
+for i in range(10):
+    va = array[i] % 42
+    value.append(va)
+
+value = set(value)
+print(len(value))
+
+# #study
+
+# #숏코딩
+# value = {int(input())%42 for _ in range(10)}
+# print(len(value))
+
+# # 튜플로 풀어보기
+# value = (int(input())%42 for _ in range(10))
+# print(len(value))
+# # TypeError: object of type 'generator' has no len()
+# #리스트로 다시 변경해주면 통과
+# value = (int(input())%42 for _ in range(10))
+# print(lenlist((value)))
